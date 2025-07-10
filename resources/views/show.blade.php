04c0e96a3a61884eb947b6420e948bca4ff4262b
Muhammed <muhammed@intelico.su>
2025-07-10 15:22:27 +0300

    fixed url in js and css files

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,17 +4,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="icon" href="/blanket/assets/favicon-32x32.png">
+    <link rel="icon" href="/proxy/storage/blanket/assets/favicon-32x32.png">
     <title>Laravel Blanket</title>
     <script>
         window.blanket_logs_per_page = '{{ config('blanket.logs_per_page') }}'
         window.blanket_base_url = '{{ url(config('blanket.path')) }}'
         window.blanket_app_env = '{{ app()->environment() }}'
     </script>
-    <script defer="defer" src="{{asset('/vendor/blanket/js/chunk-vendors.js')}}" type="module"></script>
-    <script defer="defer" src="{{asset('/vendor/blanket/js/app.js')}}" type="module"></script>
-    <link href="{{asset('/vendor/blanket/css/chunk-vendors.css')}}" rel="stylesheet">
-    <link href="{{asset('/vendor/blanket/css/app.css')}}" rel="stylesheet">
+    <script defer="defer" src="{{asset('/proxy/storage/blanket/js/chunk-vendors.js')}}" type="module"></script>
+    <script defer="defer" src="{{asset('/proxy/storage/blanket/js/app.js')}}" type="module"></script>
+    <link href="{{asset('/proxy/storage/blanket/css/chunk-vendors.css')}}" rel="stylesheet">
+    <link href="{{asset('/proxy/storage/blanket/css/app.css')}}" rel="stylesheet">
 </head>
 <body>
 <noscript><strong>We're sorry but app doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
